c40b9609ce6090c77263d15f263ed95f00384764
Tanuj Agarwal <tanujag.2531@gmail.com>
2026-03-16 13:44:53 +0530

    adding account section to see the account the user create

---
+++ PopX/src/pages/Account.jsx
@@ -0,0 +1,16 @@
+function Account() {
+  return (
+    <div className="container">
+      <h2>Account Settings</h2>
+
+      <img src="https://randomuser.me/api/portraits/women/44.jpg" width="80" />
+
+      <h3>Marry Doe</h3>
+      <p>marry@gmail.com</p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+}
+
+export default Account;
