39a6b700048db166d9793573945d069bbe3726fd
Tanuj Agarwal <tanujag.2531@gmail.com>
2026-03-16 13:36:37 +0530

    adding the welcome page for register or login the user

---
+++ PopX/src/pages/Welcome.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+function Welcome() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <h2>Welcome to PopX</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+      <button className="primary" onClick={() => navigate("/register")}>
+        Create Account
+      </button>
+
+      <button className="secondary" onClick={() => navigate("/login")}>
+        Already Registered? Login
+      </button>
+    </div>
+  );
+}
+
+export default Welcome;
